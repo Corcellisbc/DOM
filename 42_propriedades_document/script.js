fc979c8e72bdf81df90baeb3971c361199cb360b
Corcellisbc <guilhermecorcelli.sbc@gmail.com>
2024-07-12 14:58:38 -0300

    DOM

---
+++ 42_propriedades_document/script.js
@@ -0,0 +1,13 @@
+console.log(document.body);
+
+console.log(document.links);
+
+document.links[0].textContent = "Twitter"
+
+console.log(document.URL);
+
+console.log(document.title);
+
+document.title = "aula 42"
+
+console.log(document.title);
---
+++ 43_callbacks/script.js
@@ -0,0 +1,11 @@
+const soma = (a, b) => {
+  let op = a + b;
+  console.log(op);
+};
+
+const multiplicacao = (a, b) => {
+  let op = a * b;
+  console.log(op);
+};
+
+multiplicacao(10, 3^3);
